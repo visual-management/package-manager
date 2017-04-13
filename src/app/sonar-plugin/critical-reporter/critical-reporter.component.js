3221044af7442ebff06566a7ca100a6d129657d7
LE LAY Olivier <liollury@gmail.com>
2017-04-13 16:47:52 +0200

    (feat(sonar): plug critical reporter

--- src/app/sonar-plugin/critical-reporter/critical-reporter.component.js
+++ src/app/sonar-plugin/critical-reporter/critical-reporter.component.js
@@ -1,2 +1,41 @@
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
+
 export default {
-}
+  props: {
+    config: {
+      url: String,
+      projectId: Number
+    }
+  },
+  computed: {
+    url: {
+      get() {
+        return this.config.url.replace('{projectId}', this.config.projectId);
+      }
+    }
+  },
+  data() {
+    return {
+      nbViolation: 0
+    }
+  },
+  created () {
+    this.update();
+    setInterval(this.update.bind(this), 60000);
+  },
+  methods: {
+    update () {
+      this.$http.get(this.url).then((data) => {
+        for(let msr of data.body[0].msr) {
+          if (msr.key === 'critical_violations') {
+            this.nbViolation = msr.val;
+          }
+        }
+      });
+
+
+    }
+  }
+};
